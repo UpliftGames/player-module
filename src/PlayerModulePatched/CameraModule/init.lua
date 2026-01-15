89b16bf7dfebcd8888a70ad22bb0977c716859a3
EgoMoose <EgoMoose@users.noreply.github.com>
2026-01-15 00:34:38 +0000

    (0.704.0.7041059)

--- src/PlayerModulePatched/CameraModule/init.lua
+++ src/PlayerModulePatched/CameraModule/init.lua
@@ -114,7 +114,6 @@ end
 
 local FFlagUserPlayerConnectionMemoryLeak = FlagUtil.getUserFlag("UserPlayerConnectionMemoryLeak")
 local FFlagUserPSFixCameraControllerReset = FlagUtil.getUserFlag("UserPSFixCameraControllerReset")
-local FFlagUserCheckTouchControlMode = FlagUtil.getUserFlag("UserCheckTouchControlMode")
 
 -- Change this later as types are added for more classes
 type Generic = any
@@ -546,7 +545,7 @@ function CameraModule:OnLocalPlayerCameraPropertyChanged(propertyName: string)
 end
 
 function CameraModule:OnUserGameSettingsPropertyChanged(propertyName: string)
-	if propertyName == "ComputerCameraMovementMode" or (FFlagUserCheckTouchControlMode and propertyName == "TouchCameraMovementMode") then
+	if propertyName == "ComputerCameraMovementMode" or propertyName == "TouchCameraMovementMode" then
 		self:ActivateCameraController()
 	end
 end
