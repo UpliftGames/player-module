ec303f653d1d46e07f0308f41f32ff500c238b0d
EgoMoose <EgoMoose@users.noreply.github.com>
2026-07-15 00:42:30 +0000

    (0.730.0.7300790)

--- src/PlayerModulePatched/CameraModule/init.lua
+++ src/PlayerModulePatched/CameraModule/init.lua
@@ -427,6 +427,9 @@ function CameraModule:ActivateCameraController()
 	if self.activeCameraController then
 		-- deactivate the old controller and activate the new one
 		if self.activeCameraController ~= newCameraController then
+			if newCameraController.HandleSubjectDistance then
+				newCameraController:HandleSubjectDistance(self.activeCameraController)
+			end
 			self.activeCameraController:Enable(false)
 			self.activeCameraController = newCameraController
 			self.activeCameraController:Enable(true)
@@ -566,7 +569,7 @@ function CameraModule:Update(dt)
 
 		local newCameraCFrame, newCameraFocus = self.activeCameraController:Update(dt)
 
-		if self.activeOcclusionModule then
+		if self.activeOcclusionModule and not self.activeCameraController.skipOcclusion then
 			newCameraCFrame, newCameraFocus = self.activeOcclusionModule:Update(dt, newCameraCFrame, newCameraFocus)
 		end
 
